92cbbc30eae534352185fa179f0b29e221fc3be9
Ahmet <ahmet1aydos@gmail.com>
2023-01-21 12:17:57 +0300

    yusufabimders

---
+++ HTML/js/app.js
@@ -0,0 +1,7 @@
+const dugme = document.getElementById("button")
+console.log(dugme)
+let input = document.getElementsByTagName("input")
+
+dugme.addEventListener("click",function(){
+    console.log("evet çalışıyor")
+})
