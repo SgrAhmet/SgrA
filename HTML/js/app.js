8005daad132aa36181c626de682efdec18769111
Ahmet <ahmet1aydos@gmail.com>
2023-01-25 00:31:00 +0300

    bootstrap

--- HTML/js/app.js
+++
@@ -1,7 +0,0 @@
-const dugme = document.getElementById("button")
-console.log(dugme)
-let input = document.getElementsByTagName("input")
-
-dugme.addEventListener("click",function(){
-    console.log("evet çalışıyor")
-})
