d31a64e75146ff13e4c21e2c59c7de831a5f3c63
Ahmet <ahmet1aydos@gmail.com>
2023-02-13 23:06:39 +0300

    guess game

--- JS/guessthenumber/script.js
+++ JS/guessthenumber/script.js
@@ -1,4 +1,47 @@
 let num = Math.floor(Math.random() * 100)  + 1
-console.log(num)
 
-let guess = prompt
+
+
+
+let guess = Number(prompt("Please Enter Your Guess"))
+
+
+
+
+
+
+
+if(guess<num){
+
+    alert("Your Guess is lower")
+    let guess0 = prompt("Guess Again")
+
+    if(guess0<num){
+        alert("Your Guess is lower")
+        let guess1 = prompt("Guess Again")
+
+        if(guess1!==num){
+            alert("You Lost")
+                    } else {
+
+                            alert("You Win")
+                    }
+        
+
+        } else if(guess0>num) {
+            alert("Your Guess is higher")
+            let guess2 = prompt("Guess Again")
+            if(guess2!==num){
+                alert("You Lost")
+            } else{
+                alert("You Win")
+            }
+
+        }
+
+} else if(guess>num){
+    alert("Your Guess is higher")
+} else {
+
+    alert("Reyizim sen kahin misin")
+}
