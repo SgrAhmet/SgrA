83085585184444fb36460f2fbfda3ca260632f83
Ahmet <ahmet1aydos@gmail.com>
2023-02-22 14:29:39 +0300

    areafinal

---
+++ JS/functionhomeworks/script.js
@@ -0,0 +1,17 @@
+
+// ?! --------------Alan Hesabı---------
+// const alan = (a,h) => (a*h)/2;
+// alert(alan(2,9))
+// ?! --------------Kare Küp üs Hesabı---------
+
+// const kareAl = (num) => num ** 2;
+
+// const küpAl = (num) => num ** 3;
+
+// const üsAl = (num) => num ** num;
+
+// let num = Number(prompt("Please Enter A Number"))
+
+// alert (`Karesi : ${kareAl(num)} Küpü : ${küpAl(num)} Üssü : ${üsAl(num)}`)
+
+
