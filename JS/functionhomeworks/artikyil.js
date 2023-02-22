07d54f1a99cf20727c1f56267e7cda04c3d6473f
Ahmet <ahmet1aydos@gmail.com>
2023-02-22 15:09:31 +0300

    artikyearfinal

--- JS/functionhomeworks/artikyil.js
+++ JS/functionhomeworks/artikyil.js
@@ -1,15 +1,40 @@
+// ?Genel bir kural olarak, artık yıllar 4 rakamının katı olan yıllardır:
+
+//? 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044, 2048 vb.
+//? Ancak bu kuralın bir istisnası vardır:
+
+//? 1. 100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
+
+//? Örneğin 1600 ve 2000 yılları artık yıldır ancak 1800 ve 1900 artık yıl değildir.
+
+
 const artikyil = function(year){
 
     if(year % 4 === 0){
         
-        if(year % 100 === 0 && year % 400 === 0 ){
+        // if(year % 100 === 0 && year % 400 === 0 ){
+        //     alert("Artık Yıl")
+        // }
+
+        if(year % 100 === 0 && year % 400 !== 0){
+
+            alert("Artık Yıl Değil bas")
+            
+
+        }else{
             alert("Artık Yıl")
         }
 
+        
+
 
     }else{
-        alert("Artık Yıl Değil")
+        alert("Artık Yıl Değil son")
     }
 
 
 }
+
+let year = Number(prompt("Yıl Giriniz"))
+artikyil(year);
+
