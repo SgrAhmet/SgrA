6bc63a346f866d13b181f9c9ec1379340e6701e9
Ahmet <ahmet1aydos@gmail.com>
2023-03-02 16:15:26 +0300

    strings1

---
+++ JS/JS08/script.js
@@ -0,0 +1,18 @@
+let str = `Hello World`
+// console.log(str)
+
+// console.log(str.length)
+
+// console.log(str[0])
+// console.log(str[1])
+// console.log(str[2])
+// console.log(str[3])
+// console.log(str[4])
+
+// console.log(str[str.length-1])
+
+for(let i = 0 ; i < str.length ; i++ ){
+
+    console.log(str[i])
+
+}
