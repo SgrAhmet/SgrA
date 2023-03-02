16320f3a2cf4f44c29884c1767157e27b606ca0a
Ahmet <ahmet1aydos@gmail.com>
2023-03-02 17:15:50 +0300

    break

--- JS/JS08/script.js
+++ JS/JS08/script.js
@@ -18,5 +18,27 @@ let str = `Hello World`
 // }
 
 for(let a = (str.length-1); a >= 0 ; a--){
-    console.log(str[a])
+    console.log(str[a],a)
 }
+
+// console.log(str.slice(1,8))
+// console.log(str.slice(4,7))
+// console.log(str.slice(3))
+// console.log(str.slice(-6,-2))
+
+// console.log(str.slice(2,4))
+// console.log(str.slice(4,2))
+
+// console.log(str.substring(2,4))
+// console.log(str.substring(4,2))
+
+console.log(str.includes(`World`))
+console.log(str.includes(`ahmet`))
+console.log(str.includes(`Hello`))
+console.log(str.includes(`hello`))
+
+console.log(str.indexOf(`World`))
+// ? Kaçıncı Karakterse Onu Döndürür (0,1,2,3....)
+
+console.log(str.indexOf(`l`))
+// ? Denemkki İLk Karakteri ALıyo
