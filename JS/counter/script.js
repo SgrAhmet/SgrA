5e96caf0d541cfb23394cd36c988d423666e30be
Ahmet <ahmet1aydos@gmail.com>
2023-03-07 00:50:12 +0300

    stylestart

--- JS/counter/script.js
+++ JS/counter/script.js
@@ -1,29 +1,7 @@
-// document.querySelector("#input").addEventListener("click", () => {
-//   document.querySelector("#input").innerHTML = "";
-// });
 
-// let input = document.querySelector("#input");
-// console.log(input);
-// let deneme = input.innerHTML;
-// console.log(deneme);
-// let metin;
-// function ahmet() {
-//   metin = document.querySelector("#input").value;
-
-//   console.log(metin);
-// document.getElementById("#deneme").innerHTML = metin
-
-// }
-
-
-let metin = "ahmet tam bir dangalak hıyarı"
-
-console.log(metin)
-
-let words = metin.split(` `)
-
-console.log(`Words Length : ${words.length}`)
-
-let letters = metin.split(``)
-
-console.log(`Letters Legnth : ${letters.length}`)
+function count(){
+    var metin = document.getElementById("text").value;
+    let words = metin.split(` `);
+    let letters = metin.split(``);
+    document.getElementById("demo").innerHTML = `Letters Legnth : ${letters.length} Words Length : ${words.length}`;
+}
