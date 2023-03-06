cc623ef777f75d731807d67d49fdffc8374b793d
Ahmet <ahmet1aydos@gmail.com>
2023-03-07 00:28:06 +0300

    counter

---
+++ JS/counter/script.js
@@ -0,0 +1,29 @@
+// document.querySelector("#input").addEventListener("click", () => {
+//   document.querySelector("#input").innerHTML = "";
+// });
+
+// let input = document.querySelector("#input");
+// console.log(input);
+// let deneme = input.innerHTML;
+// console.log(deneme);
+// let metin;
+// function ahmet() {
+//   metin = document.querySelector("#input").value;
+
+//   console.log(metin);
+// document.getElementById("#deneme").innerHTML = metin
+
+// }
+
+
+let metin = "ahmet tam bir dangalak hıyarı"
+
+console.log(metin)
+
+let words = metin.split(` `)
+
+console.log(`Words Length : ${words.length}`)
+
+let letters = metin.split(``)
+
+console.log(`Letters Legnth : ${letters.length}`)
