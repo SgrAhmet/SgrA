d712142ba146d13ae53e6cb9ad101a0c5d8a1dc8
Ahmet <ahmet1aydos@gmail.com>
2023-03-07 18:55:22 +0300

    js

--- JS/counter/script.js
+++ JS/counter/script.js
@@ -1,7 +1,68 @@
 
-function count(){
+function myCount(){
     var metin = document.getElementById("text").value;
     let words = metin.split(` `);
     let letters = metin.split(``);
-    document.getElementById("demo").innerHTML = `Letters Legnth : ${letters.length} Words Length : ${words.length}`;
+    // document.getElementById("words").innerHTML = `${words.length}`;
+    // document.getElementById("letter").innerHTML = `${letters.length}`;
 }
+
+
+
+function countToWords(){
+
+    var metin = document.getElementById("text").value;
+    let words = metin.split(` `);
+    let letters = metin.split(``);
+    // document.getElementById("words").innerHTML = `${words.length}`;
+    // document.getElementById("letter").innerHTML = `${letters.length}`;
+
+
+
+
+    let from = 0 ;
+    let to = words.length;
+    let step = to > from ? 1 : -1;
+    let interval = words.length;
+
+    if(from == to){
+        document.querySelector("#words").textContent = from;
+        return;
+    }
+
+    let counter = setInterval(function(){
+        from += step;
+        document.querySelector("#words").textContent = from;
+
+        if(from == to){
+            clearInterval(counter);
+        }
+    }, interval);
+
+
+
+    let from1 = 0 ;
+    let to1 = letters.length;
+    let step1 = to1 > from1 ? 1 : -1;
+    let interval1 = 3;
+
+    if(from1 == to1){
+        document.querySelector("#letter").textContent = from1;
+        return;
+    }
+
+    let counter1 = setInterval(function(){
+        from1 += step1;
+        document.querySelector("#letter").textContent = from1;
+
+        if(from1 == to1){
+            clearInterval(counter1);
+        }
+    }, interval1);
+
+
+
+
+}
+
+
