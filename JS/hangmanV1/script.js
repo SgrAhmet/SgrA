cf72e614f85f8486fb51163f7eb4b06188360f06
Ahmet <ahmet1aydos@gmail.com>
2023-03-24 16:29:52 +0300

    todaybegan

--- JS/hangmanV1/script.js
+++ JS/hangmanV1/script.js
@@ -1,67 +1,37 @@
 // let words = ["ahmet","araba","kedi","laptop","lamba","bebek","priz"]
 // console.log(words)
 
- 
 // // ? rasgele kelime seçici
 // let theWord = words[Math.floor(Math.random() * (words.length ))]
 
 // console.log(theWord)
 
-let area = []
-let right = 0
-let theWord = "araba"
-
-
-
-
+let area = [];
+let right = 0;
+let theWord = "araba";
 
 theWord.split("").map((item) => {
-
-    area.push("_")
-    
-})
-
-
-
-
-
-
-
-
+  area.push("_");
+});
 
 document.getElementById("dom").innerHTML = area;
 
+while (right < 6 && area.includes("_")) {
+  let guess = prompt(`please enter guess`);
 
-while(right < 6 && area.includes("_") ){
+  if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+    right++;
+  } else {
+    right++;
+  }
 
-let guess = prompt(`please enter guess`)
-
-if(theWord.includes(guess)){
-
-    for(let i = 0;i< theWord.length;i++){
-
-
-        area[theWord.indexOf(guess,i)] = guess
-
-       
-
-} 
- right++
-    
-} else {
-
-    right++
+  document.getElementById("dom").innerHTML = area;
 }
 
-document.getElementById("dom").innerHTML = area;
-} 
+console.log(`ahmet hıyardr`);
 
-    console.log(`ahmet hıyardr`)
-
-
-
-
-
-
-console.log(right)
-console.log(area.join(" "),theWord)
+console.log(right);
+console.log(area.join(" "), theWord);
