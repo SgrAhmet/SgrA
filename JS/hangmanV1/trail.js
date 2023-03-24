8efd8c7c9e93253ddc5762e0bfe26a3f410068dc
Ahmet <ahmet1aydos@gmail.com>
2023-03-24 19:05:05 +0300

    functionsend

--- JS/hangmanV1/trail.js
+++ JS/hangmanV1/trail.js
@@ -13,47 +13,530 @@ console.log(area.join(" "))
 let guess = null
 
 console.log(guess)
-function deneme(){
 
+
+function game1(){
+
+  if(right < 6){
     guess = "a"
-console.log(guess)
-console.log(right)
-console.log(area)
 if (theWord.includes(guess)) {
     for (let i = 0; i < theWord.length; i++) {
       area[theWord.indexOf(guess, i)] = guess;
     }
-    right++;
   } else {
     right++;
   }
 document.getElementById("dom").innerHTML = area.join(" ");
 document.getElementById("right").innerHTML = 6 - right;
-
 document.getElementById("1").classList.add("ahmet")
-
-
+  }else{
+    alert("hakkınız kalmadı")
+  }
 }
-function deneme2(){
+function game2(){
 
-    guess = "f"
-console.log(guess)
-console.log(right)
-console.log(area)
+  if(right < 6){
+    guess = "b"
 if (theWord.includes(guess)) {
     for (let i = 0; i < theWord.length; i++) {
       area[theWord.indexOf(guess, i)] = guess;
     }
-    right++;
   } else {
     right++;
   }
-  document.getElementById("dom").innerHTML = area.join(" ");
-  document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("2").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game3(){
 
-  document.getElementById("2").classList.add("ahmet")
+  if(right < 6){
+    guess = "c"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("3").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game4(){
+
+  if(right < 6){
+    guess = "ç"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("4").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game5(){
+
+  if(right < 6){
+    guess = "d"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("5").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game6(){
+
+  if(right < 6){
+    guess = "e"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("6").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game7(){
+
+  if(right < 6){
+    guess = "f"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("7").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game8(){
+
+  if(right < 6){
+    guess = "g"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("8").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game9(){
+
+  if(right < 6){
+    guess = "ğ"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("9").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game10(){
+
+  if(right < 6){
+    guess = "h"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("10").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game11(){
+
+  if(right < 6){
+    guess = "ı"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("11").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game12(){
+
+  if(right < 6){
+    guess = "i"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("12").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game13(){
+
+  if(right < 6){
+    guess = "j"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("13").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game14(){
+
+  if(right < 6){
+    guess = "k"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("14").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game15(){
+
+  if(right < 6){
+    guess = "l"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("15").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game16(){
+
+  if(right < 6){
+    guess = "m"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("16").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game17(){
+
+  if(right < 6){
+    guess = "n"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("17").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game18(){
+
+  if(right < 6){
+    guess = "o"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("18").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game19(){
+
+  if(right < 6){
+    guess = "ö"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("19").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game20(){
+
+  if(right < 6){
+    guess = "p"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("20").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game21(){
+
+  if(right < 6){
+    guess = "r"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("21").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game22(){
+
+  if(right < 6){
+    guess = "s"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("22").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game23(){
+
+  if(right < 6){
+    guess = "ş"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("23").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game23(){
+
+  if(right < 6){
+    guess = "t"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("23").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game24(){
+
+  if(right < 6){
+    guess = "u"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("24").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game25(){
+
+  if(right < 6){
+    guess = "ü"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("25").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game26(){
+
+  if(right < 6){
+    guess = "v"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("26").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game27(){
+
+  if(right < 6){
+    guess = "y"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("27").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
+}
+function game28(){
+
+  if(right < 6){
+    guess = "z"
+if (theWord.includes(guess)) {
+    for (let i = 0; i < theWord.length; i++) {
+      area[theWord.indexOf(guess, i)] = guess;
+    }
+  } else {
+    right++;
+  }
+document.getElementById("dom").innerHTML = area.join(" ");
+document.getElementById("right").innerHTML = 6 - right;
+document.getElementById("28").classList.add("ahmet")
+  }else{
+    alert("hakkınız kalmadı")
+  }
 }
 
-// document.getElementById("2").classList.add("ahmet")
 
-// document.querySelectorAll(".letter").addEventListener ( "click" ,document.querySelectorAll(".letter").classList.add("ahmet"))
+
