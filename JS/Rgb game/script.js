0dff3baf20d6c45402fabbfdce50fe049d240660
Ahmet <ahmet1aydos@gmail.com>
2023-04-04 23:04:23 +0300

    rgb game

---
+++ JS/Rgb game/script.js
@@ -0,0 +1,11 @@
+// var styles = `
+// .container{ 
+// background-color: red;
+// }
+
+
+// `
+
+// var styleSheet = document.createElement("style")
+// styleSheet.innerText = styles
+// document.head.appendChild(styleSheet)
