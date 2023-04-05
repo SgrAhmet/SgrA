f2a558d934c2f197d81ec37c516b76e33a7db27f
Ahmet <ahmet1aydos@gmail.com>
2023-04-05 12:21:44 +0300

    rgbgamerandom

--- JS/Rgb game/script.js
+++ JS/Rgb game/script.js
@@ -1,11 +1,34 @@
+let a = Math.ceil(Math.random() * 255);
+let b = Math.ceil(Math.random() * 255);
+let c = Math.ceil(Math.random() * 255);
+
 // var styles = `
-// .container{ 
-// background-color: red;
+// .colors div{ 
+// background-color: rgb(${a},${b},${c});
 // }
 
+// `;
+// var styleSheet = document.createElement("style");
+// styleSheet.innerText = styles;
+// document.head.appendChild(styleSheet);
 
-// `
 
-// var styleSheet = document.createElement("style")
-// styleSheet.innerText = styles
-// document.head.appendChild(styleSheet)
+let text = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+  Math.random() * 255
+)},${Math.ceil(Math.random() * 255)}`;
+
+var styles = `
+.colors div{ 
+background-color: rgb(${text});
+}
+
+`;
+var styleSheet = document.createElement("style");
+styleSheet.innerText = styles;
+document.head.appendChild(styleSheet);
+
+
+
+
+document.getElementById("text").innerHTML = text;
+
