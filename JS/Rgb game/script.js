a9e33f4be65059d11fc828126c46ea0e1d2b2973
Ahmet <ahmet1aydos@gmail.com>
2023-04-09 00:08:35 +0300

    rgbgamefunctions

--- JS/Rgb game/script.js
+++ JS/Rgb game/script.js
@@ -1,36 +1,68 @@
-let a = Math.ceil(Math.random() * 255);
-let b = Math.ceil(Math.random() * 255);
-let c = Math.ceil(Math.random() * 255);
-
-// var styles = `
-// .colors div{ 
-// background-color: rgb(${a},${b},${c});
-// }
-
-// `;
-// var styleSheet = document.createElement("style");
-// styleSheet.innerText = styles;
-// document.head.appendChild(styleSheet);
+let counter = 0
 
 
-let text = `${Math.ceil(Math.random() * 255)},${Math.ceil(
-  Math.random() * 255
-)},${Math.ceil(Math.random() * 255)}`;
+function game() {
+  let d1 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+    Math.random() * 255
+  )},${Math.ceil(Math.random() * 255)}`;
 
-var styles = `
-.colors div{ 
-background-color: rgb(${text});
+  let d2 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+    Math.random() * 255
+  )},${Math.ceil(Math.random() * 255)}`;
 
+  let d3 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+    Math.random() * 255
+  )},${Math.ceil(Math.random() * 255)}`;
+
+  let d4 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+    Math.random() * 255
+  )},${Math.ceil(Math.random() * 255)}`;
+
+  let d5 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+    Math.random() * 255
+  )},${Math.ceil(Math.random() * 255)}`;
+
+  let d6 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+    Math.random() * 255
+  )},${Math.ceil(Math.random() * 255)}`;
+
+  document.querySelector(".div1").style.backgroundColor = `rgb(${d1})`;
+  document.querySelector(".div2").style.backgroundColor = `rgb(${d2})`;
+  document.querySelector(".div3").style.backgroundColor = `rgb(${d3})`;
+  document.querySelector(".div4").style.backgroundColor = `rgb(${d4})`;
+  document.querySelector(".div5").style.backgroundColor = `rgb(${d5})`;
+  document.querySelector(".div6").style.backgroundColor = `rgb(${d6})`;
+
+  document.querySelector("#score").innerHTML = counter;
+
+
+  let divarea = [d1, d2, d3, d4, d5, d6];
+
+  let answer = divarea[Math.ceil(Math.random() * 6) - 1];
+
+  document.getElementById("text").innerHTML = `${answer}`;
+
+  
 }
 
-
-`;
-var styleSheet = document.createElement("style");
-styleSheet.innerText = styles;
-document.head.appendChild(styleSheet);
+console.log(d1)
+game();
 
 
 
+function Func1(){
 
-document.getElementById("text").innerHTML = text;
+  // console.log(d1)
+  // if(d1 == answer){
+  //     counter++
+  //     game();
 
+  // }
+  // else{
+  //   counter = 0
+  //   game();
+
+  // }
+}
+
+Func1();
--- JS/Rgb game/script2.js
+++ JS/Rgb game/script2.js
@@ -22,6 +22,8 @@ let d6 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
   Math.random() * 255
 )},${Math.ceil(Math.random() * 255)}`;
 
+let counter = 0
+
 
 document.querySelector(".div1").style.backgroundColor = `rgb(${d1})`
 document.querySelector(".div2").style.backgroundColor = `rgb(${d2})`
@@ -30,11 +32,98 @@ document.querySelector(".div4").style.backgroundColor = `rgb(${d4})`
 document.querySelector(".div5").style.backgroundColor = `rgb(${d5})`
 document.querySelector(".div6").style.backgroundColor = `rgb(${d6})`
 
+document.querySelector("#score").innerHTML = counter
+
 
 let divarea = [d1,d2,d3,d4,d5,d6]
 
 let answer = divarea[Math.ceil(Math.random() * 6) - 1 ]
 
-// Math.ceil(Math.random() * 6) - 1 
-
 document.getElementById("text").innerHTML = `${answer}`
+
+
+
+function Func1 (){
+
+  if(d1 == answer){
+      counter++
+document.querySelector("#score").innerHTML = counter
+
+  }
+  else{
+    counter--
+document.querySelector("#score").innerHTML = counter
+
+  
+  }
+}
+function Func2 (){
+
+  if(d2 == answer){
+      counter++
+document.querySelector("#score").innerHTML = counter
+
+  }
+  else{
+    counter--
+document.querySelector("#score").innerHTML = counter
+
+  
+  }
+}
+function Func3 (){
+
+  if(d3 == answer){
+      counter++
+document.querySelector("#score").innerHTML = counter
+
+  }
+  else{
+    counter--
+document.querySelector("#score").innerHTML = counter
+
+  
+  }
+}
+function Func4 (){
+
+  if(d4 == answer){
+      counter++
+document.querySelector("#score").innerHTML = counter
+
+  }
+  else{
+    counter--
+document.querySelector("#score").innerHTML = counter
+
+  
+  }
+}
+function Func5 (){
+
+  if(d5 == answer){
+      counter++
+document.querySelector("#score").innerHTML = counter
+
+  }
+  else{
+    counter--
+document.querySelector("#score").innerHTML = counter
+
+  
+  }
+}
+function Func6 (){
+
+  if(d6 == answer){
+      counter++
+document.querySelector("#score").innerHTML = counter
+
+  }
+  else{
+    counter--
+document.querySelector("#score").innerHTML = counter
+
+  
+  }
+}
