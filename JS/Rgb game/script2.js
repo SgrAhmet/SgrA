782e40d07faea5c037471f12f7619f901ec4be1a
Ahmet <ahmet1aydos@gmail.com>
2023-04-08 17:50:16 +0300

    style

---
+++ JS/Rgb game/script2.js
@@ -0,0 +1,40 @@
+let d1 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+  Math.random() * 255
+)},${Math.ceil(Math.random() * 255)}`;
+
+let d2 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+  Math.random() * 255
+)},${Math.ceil(Math.random() * 255)}`;
+
+let d3 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+  Math.random() * 255
+)},${Math.ceil(Math.random() * 255)}`;
+
+let d4 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+  Math.random() * 255
+)},${Math.ceil(Math.random() * 255)}`;
+
+let d5 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+  Math.random() * 255
+)},${Math.ceil(Math.random() * 255)}`;
+
+let d6 = `${Math.ceil(Math.random() * 255)},${Math.ceil(
+  Math.random() * 255
+)},${Math.ceil(Math.random() * 255)}`;
+
+
+document.querySelector(".div1").style.backgroundColor = `rgb(${d1})`
+document.querySelector(".div2").style.backgroundColor = `rgb(${d2})`
+document.querySelector(".div3").style.backgroundColor = `rgb(${d3})`
+document.querySelector(".div4").style.backgroundColor = `rgb(${d4})`
+document.querySelector(".div5").style.backgroundColor = `rgb(${d5})`
+document.querySelector(".div6").style.backgroundColor = `rgb(${d6})`
+
+
+let divarea = [d1,d2,d3,d4,d5,d6]
+
+let answer = divarea[Math.ceil(Math.random() * 6) - 1 ]
+
+// Math.ceil(Math.random() * 6) - 1 
+
+document.getElementById("text").innerHTML = `${answer}`
