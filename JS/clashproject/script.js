d9c600e0f705d2e5d3ad0434df6ecf3c6415b701
Ahmet <ahmet1aydos@gmail.com>
2023-04-28 22:54:37 +0300

    functions

--- JS/clashproject/script.js
+++ JS/clashproject/script.js
@@ -1,3 +1,25 @@
 //! document.title = "TH1"
+var thlevel ;
 
+function function1 (){
+    thlevel = 1
+}
+function function2 (){
+    thlevel = 2
+}
+function function3 (){
+    thlevel = 3
+}
+function function4 (){
+    thlevel = 4
+}
+function function5 (){
+    thlevel = 5
+}
+function function6 (){
+    thlevel = 6
+}
+function function7 (){
+    thlevel = 7
+}
 
