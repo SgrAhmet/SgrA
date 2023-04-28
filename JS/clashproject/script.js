910178b63ae44d7a02d8639f916968226559f02d
Ahmet <ahmet1aydos@gmail.com>
2023-04-28 19:40:13 +0300

    todaysFirst

--- JS/clashproject/script.js
+++ JS/clashproject/script.js
@@ -1,35 +1,3 @@
-function levels(th1, th2, th3) {
-  this.th1 = th1;
-  this.th2 = th2;
-  this.th3 = th3;
-}
-
-const connon = new levels(2, 4, 6);
-const archer = new levels(3, 5, 7);
-
-let mortar = {
-  th1: 1,
-  th2: 5,
-  th3: 9,
-};
-
-// console.log(mortar)
-
-mortar.th4 = 3
-
-// console.log(mortar)
-
-// console.log(Object.keys(mortar))
+//! document.title = "TH1"
 
 
-console.log(document.getElementsByTagName("button"))
-
-let buttons = document.getElementById("th1")
-
-buttons.addEventListener("click", function() {
-    
-    console.log("ahmet")
-
-  });
-
-  
