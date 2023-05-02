7daf947f754c4941599addc7303ea6afd8628e5b
Ahmet <ahmet1aydos@gmail.com>
2023-05-02 11:02:36 +0300

    allbuildings

--- JS/clashproject/script.js
+++ JS/clashproject/script.js
@@ -1,6 +1,6 @@
 //! document.title = "TH1"
 
-function Buildings(th1, th2, th3, th4, th5, th6, th7) {
+function Buildings(th1, th2, th3, th4, th5, th6, th7,th8,th9,th10,th11,th12,th13,th14) {
   this.th1 = th1;
   this.th2 = th2;
   this.th3 = th3;
@@ -8,9 +8,28 @@ function Buildings(th1, th2, th3, th4, th5, th6, th7) {
   this.th5 = th5;
   this.th6 = th6;
   this.th7 = th7;
+  this.th8 = th8;
+  this.th9 = th9;
+  this.th10 = th10;
+  this.th11 = th11;
+  this.th12 = th12;
+  this.th13 = th13;
+  this.th14 = th14;
 }
 
-const mortar = new Buildings("X", "X", 1, 2, 3, 4, 5);
+const cannon = new Buildings(2,3,4,5,6,7,8,10,11,13,15,17,19,20);
+const archer = new Buildings("X",2,3,4,5,6,7,8,10,11,13,15,17,19,20);
+const mortar = new Buildings("X","X",1,2,3,4,5,6,7,8,10,12,13,14);
+const airDefense = new Buildings("X","X","X",2,3,4,5,6,7,8,9,10,11,12);
+const wizardTower  = new Buildings("X","X","X","X",2,3,4,6,7,9,10,11,13,14);
+const airSweper  = new Buildings("X","X","X","X","X",2,3,4,5,6,7,7,7,7);
+const hiddenTesla  = new Buildings("X","X","X","X","X","X",3,6,7,8,9,10,12,13);
+const bombTower  = new Buildings("X","X","X","X","X","X","X",2,3,4,6,7,8,9);
+const xBow  = new Buildings("X","X","X","X","X","X","X","X",3,4,5,6,8,9);
+const infernoTower  = new Buildings("X","X","X","X","X","X","X","X","X",3,5,6,7,8);
+const eagleArtillery  = new Buildings("X","X","X","X","X","X","X","X","X","X",2,3,4,5);
+const scattershot  = new Buildings("X","X","X","X","X","X","X","X","X","X","X","X",2,3);
+
 
 var thlevel;
 
@@ -57,9 +76,11 @@ function function14() {
   thlevel = 14;
 }
 
-console.log(mortar);
+
 
 function mainFunction() {
   document.getElementById("mortar").innerHTML = `${mortar[`th${thlevel}`]}`;
+
+  
 }
-ç
+
