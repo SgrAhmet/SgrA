6d097baf3c8e5d4067eaaeff0791825750d6c918
Ahmet <ahmet1aydos@gmail.com>
2023-05-16 19:00:50 +0300

    todoLocaleStrge

--- JS/toDoApp v2.0/app.js
+++ JS/toDoApp v2.0/app.js
@@ -1,3 +1,57 @@
-const htmlUl = document.getElementById("main-ul")
-// const localeStorageList = localStorage.setItem("ahmet",1)
-// console.log(localStorage.getItem("ahmet"))
+const htmlUl = document.getElementById("main-ul");
+const addButton = document.getElementById("add-button");
+const myInput = document.getElementById("input");
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+
+
+addButton.addEventListener("click", () => {
+    // if(myInput.innerText === ""){
+
+    //     alert("Please Enter New Todo")
+    //     return;
+    // }
+
+  const newList = {
+    id: new Date().getTime(),
+    name: myInput.value,
+    checked: false,
+  };
+  myInput.value = "";
+  myInput.focus();
+
+  todoList.push(newList)
+  localStorage.setItem("todoList",JSON.stringify(todoList))
+  createNewElement(newList)
+});
+
+myInput.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) {
+    addButton.click();
+  }
+});
+
+const createNewElement = (newList)=>{
+    const {id ,name ,checked} = newList
+    
+   
+    const newLi = document.createElement("li")
+    newLi.setAttribute("id",id)
+    checked ? newLi.classList.add("checked") : null;
+    htmlUl.appendChild(newLi)
+
+
+     const newCheckBox = document.createElement("i")
+    newCheckBox.setAttribute("class","fa-regular fa-square-check")
+    newLi.appendChild(newCheckBox)
+
+    const newP = document.createElement("p")
+    newP.innerText = name
+    newLi.appendChild(newP)
+
+    const newTrashBtn = document.createElement("i")
+    newTrashBtn.setAttribute("class","fa-solid fa-trash-can")
+    newLi.appendChild(newTrashBtn)
+    
+    console.log(newLi)
+
+}
