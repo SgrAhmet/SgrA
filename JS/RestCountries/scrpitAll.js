90c39dc4b7ee4b76b8a0f6d871973224cde8b8df
Ahmet <ahmet1aydos@gmail.com>
2023-07-10 10:03:58 +0300

    allCountriesDone

--- JS/RestCountries/scrpitAll.js
+++ JS/RestCountries/scrpitAll.js
@@ -1,64 +1,104 @@
-let counter = 0
-const mainDiv = document.querySelector(".mainDiv")
+let counter = 0;
+const mainDiv = document.querySelector(".mainDiv");
 
+const load = async () => {
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all").then(
+      (response) => response.json()
+    );
 
-const load = async ()=>{
-    
-    try {
+    for (let i = 0; i < 200; i++) {
+      const newCountry = document.createElement("div");
+      const newCountryName = document.createElement("h3");
+      const newCapital = document.createElement("p");
+      const newPopulation = document.createElement("p");
+      const newArea = document.createElement("p");
+      const newSup = document.createElement("sup");
+      const newCurrencie = document.createElement("p");
+      const newFlagLink = document.createElement("a");
+      const newFlag = document.createElement("img");
 
-        const response = await fetch("https://restcountries.com/v3.1/all").then(response=>response.json())
+      newCountry.classList = "country";
 
-        
+      // -------------------------------------------------------------
 
-    for (let i = 0; i < 5; i++) {
-
-        const newCountry = document.createElement("div")
-        const newCountryName = document.createElement("h3")
-        const newCapital = document.createElement("p")
-        const newPopulation = document.createElement("p")
-        const newArea = document.createElement("p")
-        const newSup = document.createElement("sup")
-        const newCurrencie = document.createElement("p")
-        const newFlagLink = document.createElement("a")
-        const newFlag = document.createElement("img")
-        
-        
-        newCountry.classList = "country"
-        newCountryName.innerText = `${response[i].name.common}`
-        newCapital.innerText = `Capital : ${response[i].capital[0]}`
-        newPopulation.innerText = `Population : ${response[i].population}`
-        newArea.innerText = `Area : ${response[0].area}km`
-        newSup.innerText = `2`
-        newCurrencie.innerText = `Currencie : ${Object.values(response[i].currencies)[0].name} (${Object.values(response[i].currencies)[0].symbol})`
-        newFlagLink.href = `${response[i].maps.googleMaps}`
-        newFlagLink.target = "_blank"
-        newFlag.src = `${response[i].flags.png}`
-
-        
-        
-         mainDiv.appendChild(newCountry)
-         newCountry.appendChild(newCountryName)
-         newCountry.appendChild(newCapital)
-         newCountry.appendChild(newPopulation)
-         newCountry.appendChild(newArea)
-         newArea.appendChild(newSup)
-         newCountry.appendChild(newCurrencie)
-         newCountry.appendChild(newFlagLink)
-         newFlagLink.appendChild(newFlag)
-        
-     
+      if (response[i].name.common) {
+        newCountryName.innerText = `${response[i].name.common}`;
+      } else {
+        newCountryName.innerText = `None`;
       }
-  
 
+      // -------------------------------------------------------------
 
-    } catch (error) {
-        
+      if (response[i].capital) {
+        newCapital.innerText = `Capital : ${response[i].capital[0]}`;
+      } else {
+        newCapital.innerText = `Capital : None`;
+      }
+
+      // -------------------------------------------------------------
+      if (response[i].population) {
+        newPopulation.innerText = `Population : ${response[i].population}`;
+      } else {
+        newPopulation.innerText = `Population : None`;
+      }
+      // -------------------------------------------------------------
+      if (response[i].area) {
+        newArea.innerText = `Area : ${response[0].area}km`;
+      } else {
+        newArea.innerText = `Area : None km`;
+      }
+      // -------------------------------------------------------------
+
+      newSup.innerText = `2`;
+      // -------------------------------------------------------------
+      if (response[i].currencies) {
+        newCurrencie.innerText = `Currencie : ${
+          Object.values(response[i].currencies)[0].name
+        } (${Object.values(response[i].currencies)[0].symbol})`;
+      } else {
+        newCurrencie.innerText = `Currencie : None (none)`;
+      }
+
+      // -------------------------------------------------------------
+      if (response[i].maps.googleMaps) {
+        newFlagLink.href = `${response[i].maps.googleMaps}`;
+      } else {
+        newFlagLink.href = `#`;
+      }
+
+      // -------------------------------------------------------------
+
+      newFlagLink.target = "_blank";
+      // -------------------------------------------------------------
+      if (response[i].flags.png) {
+        newFlag.src = `${response[i].flags.png}`;
+      } else {
+        newFlag.src = `#`;
+        newFlag.alt = `Null`;
+      }
+
+      // -------------------------------------------------------------
+
+      mainDiv.appendChild(newCountry);
+      newCountry.appendChild(newCountryName);
+      newCountry.appendChild(newCapital);
+      newCountry.appendChild(newPopulation);
+      newCountry.appendChild(newArea);
+      newArea.appendChild(newSup);
+      newCountry.appendChild(newCurrencie);
+      newCountry.appendChild(newFlagLink);
+      newFlagLink.appendChild(newFlag);
+
+      
     }
-
-}
+  } catch (error) {
+    
+    console.log(error);
+  }
+};
 
 load();
-
 
 // const newCountry = document.createElement("div")
 // const newCountryName = document.createElement("h3")
@@ -69,7 +109,6 @@ load();
 // const newCurrencie = document.createElement("p")
 // const newFlagLink = document.createElement("a")
 // const newFlag = document.createElement("img")
-
 
 // newCountry.classList = "country"
 // newCountryName.innerText = "Poland"
@@ -82,7 +121,6 @@ load();
 // newFlagLink.target = "_blank"
 // newFlag.src = "https://flagcdn.com/w320/tr.png"
 
-
 //  mainDiv.appendChild(newCountry)
 //  newCountry.appendChild(newCountryName)
 //  newCountry.appendChild(newCapital)
@@ -92,3 +130,6 @@ load();
 //  newCountry.appendChild(newCurrencie)
 //  newCountry.appendChild(newFlagLink)
 //  newFlagLink.appendChild(newFlag)
+
+
+// document.querySelector("form input[type=submit]").onclick
